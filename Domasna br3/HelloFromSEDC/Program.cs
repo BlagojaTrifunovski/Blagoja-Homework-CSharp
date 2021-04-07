e9e9cb67c9c79b0214754cea7df7af35b88c9292
BlagojaTrifunovski <blagoja3@hotmail.com>
2021-04-07 15:39:21 +0200

    3 upload

---
+++ Domasna br3/HelloFromSEDC/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloFromSEDC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "Hello from SEDC Codeacademy 2021";
+            while (true)
+            {
+                Console.Write("Enter number: ");
+                bool isOkNum = int.TryParse(Console.ReadLine(), out int num);
+                if (isOkNum && num < testString.Length)
+                {
+                    SubStrings(testString, num);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Try again!");
+                }
+            }
+            Console.ReadLine();
+        }
+        static void SubStrings(string text, int num)
+        {
+            string newString = text.Substring(0, num);
+            Console.WriteLine($"{newString} is {newString.Length} long");
+        }
+    }
+}
---
+++ Domasna br3/Remove extra space characters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Remove_extra_space_characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringLorem = "The    best  Lorem  Ipsum        Generator in all the  sea!   Heave this   scurvy copyfiller fer yar         next   adventure  and cajol yar clients   into walking the plank with ev'ry layout!    Configure       above, then get yer pirate ipsum...own the high seas,    argh!";
+            RemoveExtraSpaceCharacters(stringLorem);
+            Console.ReadLine();
+        }
+        static void RemoveExtraSpaceCharacters(string text)
+        {
+            string[] splittedText = text.Split(" ");
+            string[] onlyWordsArray = new string[1];
+
+            for (int i = 0; i < splittedText.Length; i++)
+            {
+                if (splittedText[i] != "")
+                {
+                    onlyWordsArray[onlyWordsArray.Length - 1] = splittedText[i];
+                    Array.Resize(ref onlyWordsArray, onlyWordsArray.Length + 1);
+                }
+            }
+            Console.WriteLine(string.Join(" ", onlyWordsArray));
+        }
+    }
+}
